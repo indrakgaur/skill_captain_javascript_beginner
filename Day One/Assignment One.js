17b12502b6dba51e18010a2432e6ae10261b01cb
Indrakumar <indrakumar9.ig@gmail.com>
2023-11-08 02:42:14 +0530

    Assignment One - Multiple of Tens

---
+++ Day One/Assignment One.js
@@ -0,0 +1,9 @@
+function multipleofTens(numberGiven) {
+    if (numberGiven > 0) {
+        for (let i = 1; i <= 10; i++) {
+            console.log(numberGiven * i);
+        }
+    }
+}
+
+multipleofTens(3);
