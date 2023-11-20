2f5259b920af9d09057765880a865fc83d5695c4
Indrakumar <indrakumar9.ig@gmail.com>
2023-11-20 18:19:18 +0530

    Day Four assignment event listener

---
+++ Day Four/eventListenerAssignment.js
@@ -0,0 +1,6 @@
+let button = document.getElementById("myButton");
+
+button.addEventListener("click", function () {
+    let paragraph = document.getElementById("paragraph");
+    paragraph.textContent = "Button Clicked and content changed!";
+});
