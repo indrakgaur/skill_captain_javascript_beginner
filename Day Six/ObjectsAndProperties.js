81868f2d1a7f81ac821be63204bbf9e29df0ea32
Indrakumar <indrakumar9.ig@gmail.com>
2023-11-20 22:22:23 +0530

    Day Six : Objects and Properties

---
+++ Day Six/ObjectsAndProperties.js
@@ -0,0 +1,12 @@
+let book = {
+    title: "Fire and Ice",
+    author: "George",
+    year: 1996,
+    isAvailable: "Yes",
+}
+
+console.log("Dot Notation-");
+console.log("Title: " + book.title, " Author: " + book.author, " Year: " + book.year, " Available: " + book.isAvailable);
+
+console.log("Bracket Notation-");
+console.log("Title: " + book["title"], " Author: " + book["author"], " Year: " + book["year"], " Available: " + book["isAvailable"]);
