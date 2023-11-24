d34bda2fcdbb6b83d201d120de6101aed11d62ac
Indrakumar <indrakumar9.ig@gmail.com>
2023-11-24 21:59:47 +0530

    Event bubbling and capturing

---
+++ JS-II Day Six/script.js
@@ -0,0 +1,6 @@
+const parentList = document.getElementById('listItem');
+parentList.addEventListener('click', function (event) {
+    if (event.target.tagName === 'LI') {
+        console.log('List item clicked:', event.target.textContent);
+    }
+});
